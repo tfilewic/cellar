8b7e41c1b154e0f8f8af16598286e76144c83f24
tfilewic <tfilewic@asu.edu>
2025-05-29 15:17:58 -0600

    Add unit tests for Wine

--- models/wine.py
+++ models/wine.py
@@ -23,156 +23,156 @@ class Wine(db.Model):
     rating = db.Column(db.Integer)  #scraped upon insert/update
 
 
-@staticmethod
-def validate_quantity(quantity) -> int:
-    """Validate wine quantity.
+    @staticmethod
+    def validate_quantity(quantity) -> int:
+        """Validate wine quantity.
 
-    Args:
-        quantity: Input value to validate.
+        Args:
+            quantity: Input value to validate.
 
-    Returns:
-        int: Validated quantity.
+        Returns:
+            int: Validated quantity.
 
-    Raises:
-        ValueError: If quantity is not an integer between 0 and 1000.
-    """
+        Raises:
+            ValueError: If quantity is not an integer between 0 and 1000.
+        """
 
-    try:
-        quantity = int(quantity)
-    except (TypeError, ValueError):
-        raise ValueError(f"Invalid quantity: {quantity}.  Must be integer.")
-    MAX = 1000
-    if not 0 <= quantity <= MAX:
-        raise ValueError(f"Invalid quantity: {quantity}. Out of range 0 - {MAX}.")
-    return quantity
+        try:
+            quantity = int(quantity)
+        except (TypeError, ValueError):
+            raise ValueError(f"Invalid quantity: {quantity}.  Must be integer.")
+        MAX = 1000
+        if not 0 <= quantity <= MAX:
+            raise ValueError(f"Invalid quantity: {quantity}. Out of range 0 - {MAX}.")
+        return quantity
 
-def validate_color(color: str) -> str:
-    """Validate wine color.
+    def validate_color(color: str) -> str:
+        """Validate wine color.
 
-    Args:
-        color: Input string to validate.
+        Args:
+            color: Input string to validate.
 
-    Returns:
-        str or None: Capitalized valid color or None if blank.
+        Returns:
+            str or None: Capitalized valid color or None if blank.
 
-    Raises:
-        ValueError: If color is not one of the allowed values.
-    """
+        Raises:
+            ValueError: If color is not one of the allowed values.
+        """
 
-    if not color or not color.strip():
-        return None
-    ALLOWED = {"Red", "White", "Rosé", "Orange"}
-    color = color.capitalize()
-    if not color in ALLOWED:
-        raise ValueError(f"Invalid color: {color}")
-    return color
+        if not color or not color.strip():
+            return None
+        ALLOWED = {"Red", "White", "Rosé", "Orange"}
+        color = color.strip().capitalize()
+        if not color in ALLOWED:
+            raise ValueError(f"Invalid color: {color}")
+        return color
 
-@staticmethod
-def validate_name(name: str) -> str:
-    """Validate wine name.
+    @staticmethod
+    def validate_name(name: str) -> str:
+        """Validate wine name.
 
-    Args:
-        name: Input string to validate.
+        Args:
+            name: Input string to validate.
 
-    Returns:
-        str: Trimmed name.
+        Returns:
+            str: Trimmed name.
 
-    Raises:
-        ValueError: If name is empty or exceeds 255 characters.
-    """
+        Raises:
+            ValueError: If name is empty or exceeds 255 characters.
+        """
 
-    if not name or not name.strip():
-        raise ValueError("Name is required")
-    if len(name) > 255:
-        raise ValueError(f"Name '{name}' too long")
-    return name
+        if not name or not name.strip():
+            raise ValueError("Wine Name is required")
+        if len(name) > 255:
+            raise ValueError(f"Wine name '{name}' too long")
+        return name.strip()
 
-@staticmethod
-def validate_vintage(vintage: str) -> str:
-    """Validate wine vintage.
+    @staticmethod
+    def validate_vintage(vintage: str) -> str:
+        """Validate wine vintage.
 
-    Args:
-        vintage: Input value to validate.
+        Args:
+            vintage: Input value to validate.
 
-    Returns:
-        str: String year or 'nv'.
+        Returns:
+            str: String year or 'nv'.
 
-    Raises:
-        ValueError: If vintage is invalid or out of range.
-    """   
+        Raises:
+            ValueError: If vintage is invalid or out of range.
+        """   
+        value = vintage
+        if not vintage or str(vintage).strip().lower() == NV:
+            return NV
+        try:
+            vintage = int(vintage)
+        except (ValueError):
+            raise ValueError(f"Invalid vintage: {value}")     
+        min_year = 1800
+        max_year = datetime.now().year
+        if not min_year <= vintage <= max_year:
+            raise ValueError(f"Invalid vintage: {value}")  
+        return str(vintage)
 
-    if not vintage or vintage == NV:
-        return NV
-    try:
-        vintage = int(vintage)
-    except (ValueError):
-        raise ValueError(f"Invalid vintage: {vintage}")     
-    min_year = 1800
-    max_year = datetime.now().year
-    if not min_year <= vintage <= max_year:
-        raise ValueError(f"Invalid vintage: {vintage}")  
-    return str(vintage)
+    @staticmethod
+    def validate_varietal(varietal: str) -> str:
+        """Validate wine varietal.
 
-@staticmethod
-def validate_varietal(varietal: str) -> str:
-    """Validate wine varietal.
+        Args:
+            varietal: Input string to validate.
 
-    Args:
-        varietal: Input string to validate.
+        Returns:
+            str or None: Trimmed varietal or None if blank.
 
-    Returns:
-        str or None: Trimmed varietal or None if blank.
+        Raises:
+            ValueError: If varietal exceeds 100 characters.
+        """
 
-    Raises:
-        ValueError: If varietal exceeds 100 characters.
-    """
+        if not varietal or not varietal.strip():
+            return None
+        if len(varietal) > 100:
+            raise ValueError(f"Varietal '{varietal}' too long")
+        return varietal.strip()
 
-    if not varietal or not varietal.strip():
-        return None
-    if len(varietal) > 100:
-        raise ValueError(f"Varietal '{varietal}' too long")
-    return varietal
+    @staticmethod
+    def validate_type(type: str) -> str:
+        """Validate wine type.
 
-@staticmethod
-def validate_type(type: str) -> str:
-    """Validate wine type.
+        Args:
+            type: Input string to validate.
 
-    Args:
-        type: Input string to validate.
+        Returns:
+            str or None: Capitalized type or None if blank.
 
-    Returns:
-        str or None: Capitalized type or None if blank.
+        Raises:
+            ValueError: If type is not one of the allowed values.
+        """
+        value = type
+        if not type or not type.strip():
+            return None
+        ALLOWED = {"Still", "Sparkling", "Dessert", "Fortified"}
+        type = type.strip().capitalize()
+        if not type in ALLOWED:
+            raise ValueError(f"Invalid type: {value}")
+        return type
 
-    Raises:
-        ValueError: If type is not one of the allowed values.
-    """
-    
-    if not type or not type.strip():
-        return None
-    ALLOWED = {"Still", "Sparkling", "Dessert", "Fortified"}
-    type = type.capitalize()
-    if not type in ALLOWED:
-        raise ValueError(f"Invalid type: {type}")
-    return type
+    @staticmethod
+    def validate_producer_id(producer_id) -> int:
+        """Validate producer ID.
 
-@staticmethod
-def validate_producer_id(producer_id) -> int:
-    """Validate producer ID.
+        Args:
+            producer_id: Input value to validate.
 
-    Args:
-        producer_id: Input value to validate.
+        Returns:
+            int: Validated producer ID.
 
-    Returns:
-        int: Validated producer ID.
+        Raises:
+            ValueError: If producer_id is not a positive integer.
+        """
 
-    Raises:
-        ValueError: If producer_id is not a positive integer.
-    """
-
-    try:
-        producer_id = int(producer_id)
-    except (TypeError, ValueError):
-        raise ValueError(f"Invalid producer_id: {producer_id}.  Must be integer.")
-    if not 0 < producer_id:
-        raise ValueError(f"Invalid producer_id: {producer_id}. Must be greater than 0.") 
-    return producer_id
+        try:
+            producer_id = int(producer_id)
+        except (TypeError, ValueError):
+            raise ValueError(f"Invalid producer_id: {producer_id}.  Must be integer.")
+        if not 0 < producer_id:
+            raise ValueError(f"Invalid producer_id: {producer_id}. Must be greater than 0.") 
+        return producer_id
--- test/test_models.py
+++ test/test_models.py
@@ -12,7 +12,9 @@ from models.region import Region
 from models.producer import Producer
 from models.wine import Wine
 
+
 # Country
+
 @pytest.mark.parametrize("name", [None, "", "     "])
 def test_country_validate_name_empty(name) :
     with pytest.raises(ValueError, match="Country name is required"):
@@ -28,7 +30,9 @@ def test_country_validate_name_invalid(name) :
 def test_country_validate_name_valid(name, expected) :
     assert Country.validate_name(name) == expected
     
+
 #Region
+
 @pytest.mark.parametrize("name", [None, "", "     "])    
 def test_region_validate_name_empty(name) :
     with pytest.raises(ValueError, match="Region name is required"):
@@ -44,7 +48,8 @@ def test_region_validate_name_too_long(name) :
 def test_region_validate_name_valid(name, expected) :
     assert Region.validate_name(name) == expected
 
-@pytest.mark.parametrize("id", [None, [7], object()])
+
+@pytest.mark.parametrize("id", [None, ["7"], object()])
 def test_region_validate_country_id_not_int(id) :
     with pytest.raises(ValueError, match=re.escape(f"Invalid country_id: '{id}'.  Must be integer.")):
         Region.validate_country_id(id)
@@ -58,7 +63,9 @@ def test_region_validate_country_id_not_positive(id) :
 def test_region_validate_country_id_valid(id, expected) :
     assert Region.validate_country_id(id) == expected
 
+
 #Producer
+
 @pytest.mark.parametrize("name", [None, "", "     "])
 def test_producer_validate_name_empty(name) :
     with pytest.raises(ValueError, match="Producer name is required"):
@@ -74,6 +81,7 @@ def test_producer_validate_name_too_long(name) :
 def test_producer_validate_name_valid(name, expected) :
     assert Producer.validate_name(name) == expected
 
+
 @pytest.mark.parametrize("id", [None, [7], object()])
 def test_producer_validate_region_id_not_int(id) :
     with pytest.raises(ValueError, match=re.escape(f"Invalid region_id: {id}.  Must be integer.")):
@@ -88,23 +96,108 @@ def test_producer_validate_region_id_not_positive(id) :
 def test_producer_validate_region_id_valid(id, expected) :
     assert Producer.validate_region_id(id) == expected
 
-def test_wine_validate_quantity() :
-    pass
 
-def test_wine_validate_color() :
-    pass
+#Wine
 
-def test_wine_validate_name() :
-    pass
+@pytest.mark.parametrize("quantity", [None, "", "bananas"])
+def test_wine_validate_quantity_not_int(quantity) :
+    with pytest.raises(ValueError, match=re.escape(f"Invalid quantity: {quantity}.  Must be integer.")):
+        Wine.validate_quantity(quantity)
 
-def test_wine_validate_vintage() :
-    pass
+@pytest.mark.parametrize("quantity", [-1, 1001])
+def test_wine_validate_quantity_out_of_range(quantity) :
+    with pytest.raises(ValueError, match=re.escape(f"Invalid quantity: {quantity}. Out of range 0 - 1000.")):
+        Wine.validate_quantity(quantity)
 
-def test_wine_validate_varietal() :
-    pass
+@pytest.mark.parametrize("quantity", [0, 288, 1000])
+def test_wine_validate_quantity_valid(quantity) :
+    assert Wine.validate_quantity(quantity) == quantity
 
-def test_wine_validate_type() :
-    pass
 
-def test_wine_validate_producer_id() :
-    pass
+@pytest.mark.parametrize("color", [None, "", "     "])
+def test_wine_validate_color_empty(color) :
+    assert Wine.validate_color(color) == None
+
+@pytest.mark.parametrize("color", ["Purple"])
+def test_wine_validate_color_invalid(color) :
+    with pytest.raises(ValueError, match=re.escape(f"Invalid color: {color}")):
+        Wine.validate_color(color)
+
+@pytest.mark.parametrize("color, expected", [("  red ", "Red"), ("White", "White"), 
+    ("rOSÉ", "Rosé"), ("orange", "Orange")])
+def test_wine_validate_color_valid(color, expected) :
+    assert Wine.validate_color(color) == expected
+
+
+@pytest.mark.parametrize("name", [None, "", "     "])
+def test_wine_validate_name_empty(name) :
+    with pytest.raises(ValueError, match="Wine Name is required"):
+        Wine.validate_name(name)
+
+@pytest.mark.parametrize("name", ["z" * 256])
+def test_wine_validate_name_too_long(name) :
+    with pytest.raises(ValueError, match=f"Wine name '{name}' too long"):
+        Wine.validate_name(name)
+
+@pytest.mark.parametrize("name, expected", [("z" * 255, "z" * 255), ("Insignia", "Insignia"),
+    ("  Cuvée Sir Winston Churchill   ", "Cuvée Sir Winston Churchill")])
+def test_wine_validate_name_valid(name, expected) :
+    assert Wine.validate_name(name) == expected
+
+
+@pytest.mark.parametrize("vintage", [None, "", "NV"])
+def test_wine_validate_vintage_nonvintage(vintage) :
+    assert Wine.validate_vintage(vintage) == "nv"
+
+@pytest.mark.parametrize("vintage", ["two-thousand", "1799", "2030"])
+def test_wine_validate_vintage_invalid(vintage) :
+    with pytest.raises(ValueError, match=f"Invalid vintage: {vintage}"):
+        Wine.validate_vintage(vintage)
+
+@pytest.mark.parametrize("vintage, expected", [("1800", "1800"), ("2025", "2025")])
+def test_wine_validate_vintage_valid(vintage, expected) :
+    assert Wine.validate_vintage(vintage) == expected
+
+ 
+@pytest.mark.parametrize("varietal", [None, "", "     "])
+def test_wine_validate_varietal_none(varietal) :
+    assert Wine.validate_varietal(varietal) == None
+
+@pytest.mark.parametrize("varietal", ["z" * 101])
+def test_wine_validate_varietal_too_long(varietal) :
+    with pytest.raises(ValueError, match=f"Varietal '{varietal}' too long"):
+        Wine.validate_varietal(varietal)
+
+@pytest.mark.parametrize("varietal, expected", [("  Pinot Noir ", "Pinot Noir"), ("Tempranillo", "Tempranillo")])
+def test_wine_validate_varietal_valid(varietal, expected) :
+    assert Wine.validate_varietal(varietal) == expected
+
+
+@pytest.mark.parametrize("type", [None, "", "     "])
+def test_wine_validate_type_none(type) :
+    assert Wine.validate_type(type) == None
+
+@pytest.mark.parametrize("type", ["Porto"])
+def test_wine_validate_type_invalid(type) :
+    with pytest.raises(ValueError, match=f"Invalid type: {type}"):
+        Wine.validate_type(type)
+
+@pytest.mark.parametrize("type, expected", [("still ", "Still"), (" Sparkling",  "Sparkling"), 
+    ("Dessert", "Dessert"), ("FORTIFIED", "Fortified")])
+def test_wine_validate_type_valid(type, expected) :
+    assert Wine.validate_type(type) == expected
+
+
+@pytest.mark.parametrize("id", [None, ["7"], object()])
+def test_wine_validate_producer_id_not_int(id) :
+    with pytest.raises(ValueError, match=re.escape(f"Invalid producer_id: {id}.  Must be integer.")):
+        Wine.validate_producer_id(id)
+
+@pytest.mark.parametrize("id", ["0", 0, -2341])
+def test_wine_validate_producer_id_not_positive(id) :
+    with pytest.raises(ValueError, match=re.escape(f"Invalid producer_id: {id}. Must be greater than 0.")):
+        Wine.validate_producer_id(id)
+
+@pytest.mark.parametrize("id,expected", [(1, 1), ("1", 1), (2341, 2341)])
+def test_wine_validate_producer_id_valid(id, expected) :
+    assert Wine.validate_producer_id(id) == expected
